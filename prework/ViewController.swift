07e9fb002d74785c28b882a22c5be1d8b3e2a5c4
Weijun Fan <fwrrong@163.com>
2021-02-05 14:55:05 +0800

    adding the liking feature

--- prework/ViewController.swift
+++ prework/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var BillAmountTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calculatorTip(_ sender: Any) {
+        let bill = Double(BillAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0,18, 0,2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        TipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
